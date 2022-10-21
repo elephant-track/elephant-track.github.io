92af84c5867bcf3060e049d60dbb0d3b1b4402e1
Ko Sugawara <ko.sugawara@ens-lyon.fr>
2022-10-21 15:55:23 +0200

    Add setup script for ELEPHANT server

---
+++ docs/assets/scripts/setup_elephant_server_debian.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+# ELEPHANT server Debian installation script
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+sudo python3 install_gpu_driver.py
+rm install_gpu_driver.py NVIDIA-Linux-x86_64-*.run
+curl https://get.docker.com | sh && sudo systemctl --now enable docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt update && sudo apt install -y nvidia-docker2 && sudo systemctl restart docker
+sudo usermod -aG docker $USER && newgrp docker
+sudo apt install -y git
+git clone https://github.com/elephant-track/elephant-server.git
+cd elephant-server
+make build
